38ccbce617fce2d78b6d2dca85a3e24f957a209a
SANJAIRAGUL-MURUGESAN <sanjairagulm@gmail.com>
2024-07-04 20:19:11 +0530

    RAILWAY RESERVATION FRONTEND WORK PROGRESS

--- Reservation/Train/Script/UpdateUserProfile.js
+++ Reservation/Train/Script/UpdateUserProfile.js
@@ -1,128 +1,81 @@
-const form = document.querySelector('#form')
-const username = document.querySelector('#username');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const phonenumber = document.querySelector('#phonenumber');
-const address = document.querySelector('#address');
+const username = document.getElementById('username');
+const username1 = document.getElementById('username1');
+const useremail = document.getElementById('useremail');
+const gender = document.getElementById('gender');
+const phone = document.getElementById('phone');
+const disability = document.getElementById('disability');
+const address = document.getElementById('address');
+const email1 = document.getElementById('email1');
 
 
-console.log('gvghj')
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    if(!validateInputs()){
-        e.preventDefault();
+async function populateDetails(element){
+    username.innerHTML = element.name
+    email1.innerHTML = element.email
+    username1.innerHTML = `<p><span><strong> User Name</strong></span>: ${element.name}</p>`
+    useremail.innerHTML = `<p><span><strong> User Email</strong></span>: ${element.email}</p>`
+    gender.innerHTML = `<p><span><strong> User Gender</strong></span>: ${element.gender}</p>`
+    phone.innerHTML = `<p><span><strong> User Mobile</strong></span>: (+91) ${element.phoneNumber}</p>`
+    if(element.disability === false){
+        disability.innerHTML = `<p><span><strong> Disability</strong></span>: No</p>`
+    }else{
+        disability.innerHTML = `<p><span><strong> Disability</strong></span>: Yes</p>`
     }
-})
+    address.innerHTML = `<p><span><strong> User Address</strong></span>: ${element.address}</p>`
+}
 
-function validateInputs(){
-    const usernameVal = username.value.trim()
-    const emailVal = email.value.trim();
-    const passwordVal = password.value.trim();
-    const phonenumberVal = phonenumber.value.trim();
-    const addressVal = address.value.trim();
+async function getUserDetails() {
 
-    let success = true
+    const userid = localStorage.getItem('userid')
+    if(userid == null){
+        Toastify({
+            text: "Hey User, Login to See Profile! Redirecting...",
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+            callback: function() {
+                window.open('index.html'); // Redirect after toast disappears
+            }
+        }).showToast();
+    }else{
+    // Fetch Profile from backend
+    await fetchClasses();
 
-    if(usernameVal===''){
-        success=false;
-        setError(username,'Username is required')
-    }
-    else{
-        setSuccess(username)
-    }
+    // Function to fetch classes from backend
+    async function fetchClasses() {
+        await fetch('http://localhost:5062/api/User/UserProfile', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('token'),
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(localStorage.getItem('userid'))
+        })
+        .then(async res => {
+            if (!res.ok) {
+                throw new Error('Failed to fetch classes');
+            }
+            const data = await res.json();
+            console.log(data)
+            await populateDetails(data)
+            // classesn = await populateClassesDropdown(data); // Populate dropdown with fetched data
 
-    if(emailVal===''){
-        success = false;
-        setError(email,'Email is required')
+        })
+        .catch(error => {
+            console.error('Error fetching User:', error);
+            alert('Failed to fetch User. Please try again later.');
+        });
     }
-    else if(!validateEmail(emailVal)){
-        success = false;
-        setError(email,'Please enter a valid email')
-    }
-    else{
-        setSuccess(email)
-    }
+}
+}
 
-    if(passwordVal === ''){
-        success= false;
-        setError(password,'Password is required')
-    }
-    else if(passwordVal.length<8){
-        success = false;
-        setError(password,'Password must be atleast 8 characters long')
-    }
-    else{
-        setSuccess(password)
-    }
+getUserDetails()
 
-    function validateNumber(input) {
-        const numberRegex = /^\d+$/;
-        return numberRegex.test(input);
-    }
+
+
 
  
-    if(phonenumberVal === ''){
-        success= false;
-        setError(phonenumber,'Phone Number is required')
-    }
-    else if (!validateNumber(phonenumberVal)) {
-        success= false;
-        setError(phonenumber,'Only Numbers are allowed')
-    }
-    else if(phonenumberVal.length>10){
-        success = false;
-        setError(phonenumber,'Phone Number should not Exeed 10 Numbers')
-    }
-    else if(phonenumberVal.length<10){
-        success = false;
-        setError(phonenumber,'Phone Number should not be less than 10 Numbers')
-    }
-    else{
-        setSuccess(phonenumber)
-    }
-
-    if(addressVal === ''){
-        success= false;
-        setError(address,'Address is required')
-    }
-    else if(addressVal.length<10){
-        success = false;
-        setError(address,'Address must be atleast 10 characters long')
-    }
-    else{
-        setSuccess(address)
-    }
-
  
 
 
+ 
 
-    return success;
-
-}
-//element - password, msg- pwd is reqd
-function setError(element,message){
-    const inputGroup = element.parentElement;
-    const errorElement = inputGroup.querySelector('.error')
-
-    errorElement.innerText = message;
-    inputGroup.classList.add('error')
-    inputGroup.classList.remove('success')
-}
-
-function setSuccess(element){
-    const inputGroup = element.parentElement;
-    const errorElement = inputGroup.querySelector('.error')
-
-    errorElement.innerText = '';
-    inputGroup.classList.add('success')
-    inputGroup.classList.remove('error')
-}
-
-const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
